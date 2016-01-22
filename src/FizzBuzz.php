1239a0d1eed0ec76a8272aa9e50ec51146d56c5e
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-01-22 16:58:23 +0100

    Optimization

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -5,28 +5,10 @@ class FizzBuzz
 
     public function execute($number)
     {
-        $solution = '';
+        if($number % 15 == 0) return 'fizzbuzz';
+        if($number % 5 == 0) return 'buzz';
+        if($number % 3 == 0) return 'fizz';
 
-        $divByFive = $number % 5 == 0;
-        $divByThree = $number % 3 == 0;
-
-        if($divByFive || $divByThree)
-        {
-            if($divByThree)
-            {
-                $solution .= 'fizz';
-            }
-
-            if($divByFive)
-            {
-                $solution .= 'buzz';
-            }
-        }
-        else
-        {
-            $solution = $number;
-        }
-
-        return $solution;
+        return $number;
     }
 }
