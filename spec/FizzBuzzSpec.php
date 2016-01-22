8adef55408ccde5bd689564690e3994140321af6
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-01-22 17:14:07 +0100

    Support for sequences

--- spec/FizzBuzzSpec.php
+++ spec/FizzBuzzSpec.php
@@ -10,41 +10,48 @@ class FizzBuzzSpec extends ObjectBehavior
 
     function it_translates_1_for_fizzbuzz()
     {
-        $this->execute(1)->shouldReturn(1);
+        $this->translate(1)->shouldReturn(1);
     }
 
     function it_translates_2_for_fizzbuzz()
     {
-        $this->execute(2)->shouldReturn(2);
+        $this->translate(2)->shouldReturn(2);
     }
 
     function it_translates_3_for_fizzbuzz()
     {
-        $this->execute(3)->shouldReturn('fizz');
+        $this->translate(3)->shouldReturn('fizz');
     }
 
     function it_translates_5_for_fizzbuzz()
     {
-        $this->execute(5)->shouldReturn('buzz');
+        $this->translate(5)->shouldReturn('buzz');
     }
 
     function it_translates_6_for_fizzbuzz()
     {
-        $this->execute(6)->shouldReturn('fizz');
+        $this->translate(6)->shouldReturn('fizz');
     }
 
     function it_translates_8_for_fizzbuzz()
     {
-        $this->execute(9)->shouldReturn('fizz');
+        $this->translate(9)->shouldReturn('fizz');
     }
 
     function it_translates_10_for_fizzbuzz()
     {
-        $this->execute(10)->shouldReturn('buzz');
+        $this->translate(10)->shouldReturn('buzz');
     }
 
     function it_translates_15_for_fizzbuzz()
     {
-        $this->execute(15)->shouldReturn('fizzbuzz');
+        $this->translate(15)->shouldReturn('fizzbuzz');
     }
+
+    function it_translates_a_sequence_for_fizzbuzz()
+    {
+        $this->sequence(10,15)->shouldReturn(['buzz',11,'fizz',13,14,'fizzbuzz']);
+    }
+
+
 }
--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -3,11 +3,37 @@
 class FizzBuzz
 {
 
-    public function execute($number)
+    /**
+     * @param $from
+     * @param $to
+     * @return array
+     */
+    public function sequence($from, $to)
     {
-        if($number % 15 == 0) return 'fizzbuzz';
-        if($number % 5 == 0) return 'buzz';
-        if($number % 3 == 0) return 'fizz';
+        $solution = [];
+
+        for ($i = $from; $i <= $to; $i++) {
+            $solution[] = $this->translate($i);
+        }
+
+        return $solution;
+    }
+
+    /**
+     * @param $number
+     * @return string
+     */
+    public function translate($number)
+    {
+        if ($number % 15 == 0) {
+            return 'fizzbuzz';
+        }
+        if ($number % 5 == 0) {
+            return 'buzz';
+        }
+        if ($number % 3 == 0) {
+            return 'fizz';
+        }
 
         return $number;
     }
